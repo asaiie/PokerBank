2b63474f8f2ea2d8f1dae621f81b417261eae228
asaiie <aerraguntla@gmail.com>
2023-12-19 20:48:42 -0500

    Initial Commit

---
+++ PokerBank/PokerBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokerBankApp.swift
+//  PokerBank
+//
+//  Created by Amit Erraguntla on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct PokerBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
